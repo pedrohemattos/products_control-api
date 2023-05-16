0a1529b213ac95cb62193d130aa8d2e7db37fbeb
pedrohemattos <pedrohmgmattos@gmail.com>
2023-05-16 18:05:54 -0300

    crud

---
+++ src/main/java/com/teste/primeiroexemplo/services/ProdutoService.java
@@ -0,0 +1,37 @@
+package com.teste.primeiroexemplo.services;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.teste.primeiroexemplo.model.Produto;
+import com.teste.primeiroexemplo.repository.ProdutoRepository;
+
+@Service
+public class ProdutoService {
+  @Autowired
+  private ProdutoRepository produtoRepository;
+
+  public List<Produto> obterTodos() {
+    return produtoRepository.obterTodos();
+  }
+
+  public Optional<Produto> obterPorId(Integer id) {
+    return produtoRepository.obterPorId(id);
+  }
+
+  public Produto adicionar(Produto produto) {
+    return produtoRepository.adicionar(produto);
+  }
+
+  public void deletar(Integer id) {
+    produtoRepository.deletar(id);
+  }
+
+  public Produto atualizar(Integer id, Produto produto) {
+    produto.setId(id);
+    return produtoRepository.atualizar(produto);
+  }
+}
